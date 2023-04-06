8975b85d7bb52dd8a38e684fe8a43f0abe979544
Black Cloud <tuank2301@gmail.com>
2023-04-06 18:21:06 +0700

    finish services

---
+++ src/components/Contact.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function Contact({}: Props) {
+  return (
+    <div className="section" id="contact">
+      Contact
+    </div>
+  );
+}
